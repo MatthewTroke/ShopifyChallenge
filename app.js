a240967da1508b661de4b404e8b0c4ca9fc8607b
Zack Rosen <zack@stitchlabs.com>
2018-07-21 03:04:54 -0400

    Add cors package

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const express      = require('express');
 const path         = require('path');
 const cookieParser = require('cookie-parser');
 const logger       = require('morgan');
+const cors         = require('cors')
+
 
 const indexRouter   = require('./routes/index');
 const shopifyRouter = require('./routes/shopify');
 
 const app = express();
+
+app.use(cors())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
